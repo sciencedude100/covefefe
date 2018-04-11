f0e1e2fde8d75f76b18f8243dc15aeb6a2e369ce
Ed Green <greene2@sunyit.edu>
2018-04-11 18:51:00 +0000

    fixed login script

--- login-prompt.sh
+++ login-prompt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 trap '' INT TSTP
-whiptail --backtitle "Covefefe Brewing Station v1.0" \
-       --title "Important Decision" \
-       --yesno "\n Would you like some\n   covefefe?\n" 10 30
-clear
-if [ $? -eq 0 ]; then
- ./covefefe.sh
+if (whiptail --defaultno --fullbuttons --backtitle "Covefefe Brewing Station v1.0" --title "Important Decision" --yesno "\n Would you like some\n   covefefe?\n" 10 30); then
+ ./covefefe/covefefe.sh
+ echo "YES"
+ clear
  sleep 2
  clear
+else
+ echo "NO"
 fi
-./login.sh
+exit
